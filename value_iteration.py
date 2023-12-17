df3215aafedaf2117dc23c82c98b4e188f193d0d
조하영 <103165774+JoHaYoung227@users.noreply.github.com>
2023-12-17 23:45:11 +0900

    value_iteration.py

--- value_iteration.py
+++ value_iteration.py
@@ -1,4 +1,5 @@
 # value_iteration.py
+import matplotlib.pyplot as plt
 
 # 가치 반복(Value Iteration) 알고리즘을 구현한 클래스 정의
 class ValueIteration:
@@ -17,6 +18,7 @@ class ValueIteration:
         # 각 상태의 초기 가치를 0으로 설정
         values = {state: 0 for state in states}
         iterations = 0
+        delta_values = []  # max delta 값을 저장할 리스트 추가
 
         while True:
             delta = 0
@@ -28,6 +30,7 @@ class ValueIteration:
                 # 변화의 최대값 갱신
                 delta = max(delta, abs(v - values[state]))
             iterations += 1
+            delta_values.append(delta)  # max delta 값을 리스트에 추가
             print(f"Iteration: {iterations}, Max Delta: {delta}")
             print(f"Current Value Function: {values}")
 
@@ -35,8 +38,26 @@ class ValueIteration:
             if delta < self.threshold:
                 break
 
+            # 최적의 결정론적 정책 추출
+            optimal_policy = {}
+            for state in states:
+                best_action = max(actions, key=lambda action: self.environment.get_rewards(state, action) + self.gamma * values[state])
+                optimal_policy[state] = best_action
+
+            # 결과 출력
+            print("Optimal Policy:")
+            for state, action in optimal_policy.items():
+                print(f"State: {state}, Optimal Action: {action}")
+
+            # 그래프 그리기
+            plt.plot(range(1, iterations + 1), delta_values, marker='o')
+            plt.xlabel('Iterations')
+            plt.ylabel('Max Delta')
+            plt.title('Convergence of Value Iteration')
+            plt.show()
+
 if __name__ == "__main__":
-    # 환경과 가치 반복 클래스를 사용하여 알고리즘 실행하는 코드
+    환경과 가치 반복 클래스를 사용하여 알고리즘 실행하는 코드
     data_path = "example.csv" # 여기에서 데이터 경로를 바꾸세요.
     from environment import Environment
     env = Environment(data_path)
